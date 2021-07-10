c31c253dab41d9fcc48b8c349643df6bad8c08c0
Vulquimar-Silva <vuquimar@hotmail.com>
2021-07-10 03:25:43 -0300

    up nova aula

--- events/events.js
+++ events/events.js
@@ -1,9 +1,15 @@
-const events = require('events')
+const {EventEmitter} = require('events')
 
-class Evento extends events.EventEmitter { }
+class Evento extends EventEmitter { }
 const meuEvento = new Evento()
 meuEvento.on('seguranca', (x, y) => {
   console.log(`Executando o evento 'seguranca: ${x} ${y}`)
 })
 
 meuEvento.emit('seguranca', 'userAdmin', 'Alterou Salário')
+
+meuEvento.on('encerrar', (dados) => {
+  console.log('Assinante: ' + dados)
+})
+
+meuEvento.emit('encerrar', 'Encerrando a execução da importação dos dados!')
